e9a167c0cd21d0d181e1a89a90b1747726a61d03
Milagros Avellaneda <milagrosavellaneda@192.168.1.4>
2023-11-12 20:02:45 -0300

    Log in por Mili

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+const port = 3000
+const views = path.join(__dirname, './views/')
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+  res.sendFile (views + "index.html")
+  
+})
+app.get('/login', (req, res) => {
+    res.sendFile (views + "login.html")
+    
+  })
+  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
