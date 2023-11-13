62e186aecee03c2dce526208d2036d2e43cb7469
LucasEMedina <a1398@MacBook-Pro-di-A1398.local>
2023-11-13 03:42:01 +0100

    inicio de vista de registro

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const app = express()
 const path = require('path');
 const port = 3000
 const views = path.join(__dirname, './views/')
+
 app.use(express.static("public"))
 
 app.get('/', (req, res) => {
@@ -12,7 +13,12 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
     res.sendFile (views + "login.html")
     
-  })
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile (views + "register.html")
+    
+})
   
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
