4b55aa458aa8bc538eb975157df168ac3f1ba498
LucasEMedina <lucasmed.emme@gmail.com>
2024-03-19 02:28:42 +0100

    se agregaron estilos en header y card-detail

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "gamersCostumes",
     "host": "127.0.0.1",
     "dialect": "mysql"
