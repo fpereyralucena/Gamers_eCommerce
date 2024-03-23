fdd8b29cd2c63d9d4e9172fdc7532d00aa6455af
fraroxx <luherbon@gmail.com>
2024-03-23 20:19:10 -0300

    base de datos

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,25 +1,25 @@
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password":  process.env.DB_PASSWORD,
-    "database": "gamerscostumes",
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password":  "root",
+    "database": "gamersCostumes",
+    "host": "127.0.0.1",
     "dialect": "mysql"
-
+   
   },
   "test": {
-    "username": process.env.DB_USER,
-    "password":  process.env.DB_PASSWORD,
-    "database": "gamerscostumes",
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.DB_USER,
-    "password":  process.env.DB_PASSWORD,
-    "database": "gamerscostumes",
-    "host": process.env.DB_HOST,
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
     "dialect": "mysql"
   }
 }
