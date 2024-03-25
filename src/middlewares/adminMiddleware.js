340eb32dfb4f2d67b0bdc09a856460b6f9658d12
fraroxx <luherbon@gmail.com>
2024-03-25 00:14:49 -0300

    admin middleware

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,6 +1,7 @@
 // Chequea que el usuario sea admin: Si es admin lo deja seguir, sino lo manda al profile.
 function adminMiddleware ( req, res, next) {
-    if (req.session.userLogged == 1) {
+    if (req.session.user.userEspecify_id == 1) {
+        res.locals.user = req.session.user;
         next();
     }
       return  res.send('El usuario no cuenta con los permisos necesarios');
