2cd6a9bfd5bdbec607f3bdd0569bdf62312539cb
fraroxx <luherbon@gmail.com>
2024-03-30 02:41:26 -0300

    corrigiendo detalles de css, y siguiento con el media query del menu desplegable

--- src/scripts/header.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('list').addEventListener('click', function() {
-    var nav = document.querySelector('tercerElemento');
-    nav.style.display = nav.style.display === 'none' ? 'block' : 'none';
-  });
